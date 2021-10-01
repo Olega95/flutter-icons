b50aabb082531367ed07505a497e45c2cb961ec7
Oleg Korotin <o9818154818@yandex.ru>
2021-10-01 19:18:00 +0300

    migrate to dart 2.12

--- test/main.dart
+++ test/main.dart
@@ -1,99 +1,99 @@
-import 'dart:io';
+// import 'dart:io';
 
-main(List<String> args) {
-  Directory directory = Directory("../lib/src");
-  File flutterIconFile = File('../lib/src/flutter_icons.dart');
-  List<File> files = directory.listSync().map((e)=>File(e.path)).toList();
-  String str = '''
-import 'package:flutter/material.dart';
-import 'flutter_icon_data.dart';
-class FlutterIcons {
-  FlutterIcons._();
-  ''';
-  for(var i=0;i<files.length;i++){
-    final File file = files[i];
-    if(file.path.indexOf("flutter_icon") == -1 && file.path.indexOf("icon_toggle") == -1){
-      final  List<String> lines = file.readAsLinesSync();
-      for(var k=0;k<lines.length;k++){
-        final String line = lines[k];
-        if(line.contains('static const')){
-          print(file.path);
-        var suffix = getSimple(line);
-        List lineList = line.split(" ");
-        lineList[5] = lineList[5]+'_$suffix';
-        String temp = lineList.join(" ");
-        str += '\n';
-        str += temp;
-        }
-        
-      }
-    }
+// main(List<String> args) {
+//   Directory directory = Directory("../lib/src");
+//   File flutterIconFile = File('../lib/src/flutter_icons.dart');
+//   List<File> files = directory.listSync().map((e)=>File(e.path)).toList();
+//   String str = '''
+// import 'package:flutter/material.dart';
+// import 'flutter_icon_data.dart';
+// class FlutterIcons {
+//   FlutterIcons._();
+//   ''';
+//   for(var i=0;i<files.length;i++){
+//     final File file = files[i];
+//     if(file.path.indexOf("flutter_icon") == -1 && file.path.indexOf("icon_toggle") == -1){
+//       final  List<String> lines = file.readAsLinesSync();
+//       for(var k=0;k<lines.length;k++){
+//         final String line = lines[k];
+//         if(line.contains('static const')){
+//           print(file.path);
+//         var suffix = getSimple(line);
+//         List lineList = line.split(" ");
+//         lineList[5] = lineList[5]+'_$suffix';
+//         String temp = lineList.join(" ");
+//         str += '\n';
+//         str += temp;
+//         }
 
-  }
-  str += '}';
-  flutterIconFile.writeAsStringSync(str);
-  // directory.list().forEach((file)=>print(file.path));
-}
-String getSimple(String line){
-  print(line);
-  var name1 = line.split(".")[1];
-  var name = name1.split("(")[0];
-  if(name == 'materialCommunityIcons')
-  return 'mco';
-  if(name == 'materialIcons')
-  return 'mdi';
-  if(name == 'simpleLineIcons')
-  return 'sli';
-  if(name == 'fontAwesome')
-  return 'faw';
-  if(name == 'fontAwesome5')
-  return 'faw5';
-  if(name == 'fontAwesome5Solid')
-  return 'faw5s';
-  if(name == 'fontAwesome5Brands')
-  return 'faw5d';
-  return name.substring(0,3).toLowerCase();
-}
-enum IconLib {
-  ///All Icons
-  all,
+//       }
+//     }
 
-  ///Ant Design Icons
-  ant,
+//   }
+//   str += '}';
+//   flutterIconFile.writeAsStringSync(str);
+//   // directory.list().forEach((file)=>print(file.path));
+// }
+// String getSimple(String line){
+//   print(line);
+//   var name1 = line.split(".")[1];
+//   var name = name1.split("(")[0];
+//   if(name == 'materialCommunityIcons')
+//   return 'mco';
+//   if(name == 'materialIcons')
+//   return 'mdi';
+//   if(name == 'simpleLineIcons')
+//   return 'sli';
+//   if(name == 'fontAwesome')
+//   return 'faw';
+//   if(name == 'fontAwesome5')
+//   return 'faw5';
+//   if(name == 'fontAwesome5Solid')
+//   return 'faw5s';
+//   if(name == 'fontAwesome5Brands')
+//   return 'faw5d';
+//   return name.substring(0,3).toLowerCase();
+// }
+// enum IconLib {
+//   ///All Icons
+//   all,
 
-  ///Entypo Icons
-  ent,
+//   ///Ant Design Icons
+//   ant,
 
-  ///Evil Icons
-  evi,
+//   ///Entypo Icons
+//   ent,
 
-  ///Feather Icons
-  fea,
+//   ///Evil Icons
+//   evi,
 
-  ///Font Awesome Icons
-  faw,
+//   ///Feather Icons
+//   fea,
 
-  ///Foundation Icons
-  fou,
+//   ///Font Awesome Icons
+//   faw,
 
-  ///Ionicons Icons
-  ion,
+//   ///Foundation Icons
+//   fou,
 
-  ///Material Community Icons
-  mco,
+//   ///Ionicons Icons
+//   ion,
 
-  ///Material Icons
-  mdi,
+//   ///Material Community Icons
+//   mco,
 
-  ///Octicons Icons
-  oct,
+//   ///Material Icons
+//   mdi,
 
-  ///Simple Line Icons
-  sli,
+//   ///Octicons Icons
+//   oct,
 
-  ///Zocial Icons
-  zoc,
+//   ///Simple Line Icons
+//   sli,
 
-  ///Weather Icons
-  wea
-}
+//   ///Zocial Icons
+//   zoc,
+
+//   ///Weather Icons
+//   wea
+// }
